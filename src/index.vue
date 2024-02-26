41a19e10920eccc51e1b503cbe4e7e3d80c47c01
VillaAgaete <emendez@aytoagaete.es>
2024-02-26 12:52:28 +0000

    click and expand cards

---
+++ src/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import clickExpandCard from './components/clickExpandCard.vue'
+</script>
+
+<template>
+  <div class="container">
+  </div>
+  <clickExpandCard />
+</template>
+
+<style scoped>
+</style>
